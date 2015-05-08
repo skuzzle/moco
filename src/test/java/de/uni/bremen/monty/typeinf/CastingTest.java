b6b8ff9418631ba99a2fc404bfbca0b381d6bd32
Simon Taddiken <simon.taddiken@gmail.com>
2015-05-08 20:15:41 +0200

    Add casting test

---
+++ src/test/java/de/uni/bremen/monty/typeinf/CastingTest.java
@@ -0,0 +1,52 @@
+package de.uni.bremen.monty.typeinf;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import de.uni.bremen.monty.moco.ast.CoreClasses;
+import de.uni.bremen.monty.moco.ast.declaration.ClassDeclaration;
+import de.uni.bremen.monty.moco.ast.declaration.VariableDeclaration;
+import de.uni.bremen.monty.moco.ast.declaration.typeinf.ClassType;
+import de.uni.bremen.monty.moco.ast.declaration.typeinf.Type;
+import de.uni.bremen.monty.moco.util.Monty;
+import de.uni.bremen.monty.moco.util.astsearch.Predicates;
+
+public class CastingTest extends AbstractTypeInferenceTest {
+
+    @Test
+    @Monty(
+    "class Foo<X>:\n" +
+    "     pass\n" +
+    "Object a := Foo<String>()\n" +
+    "Foo<String> b := a as Foo<String>"
+    )
+    public void testCastToGeneric() throws Exception {
+        this.compiler.compile();
+        this.compiler.assertAllTypesResolved();
+    }
+
+    @Test
+    @Monty(
+    "class Foo<X>:\n" +
+    "     pass\n" +
+    "Object a := Foo<String>()\n" +
+    "? xyz := a as Foo<String>"
+    )
+    public void testInferCastToGeneric() throws Exception {
+        this.compiler.compile();
+        final VariableDeclaration xyz = this.compiler.searchFor(VariableDeclaration.class,
+                Predicates.hasName("xyz"));
+
+        final ClassDeclaration decl = this.compiler.searchFor(ClassDeclaration.class,
+                Predicates.hasName("Foo"));
+
+        final Type expected = ClassType.classNamed("Foo")
+                .withSuperClass(CoreClasses.objectType().getType().asClass())
+                .addTypeParameter(CoreClasses.stringType().getType())
+                .createType();
+
+        assertUniqueTypeIs(expected, xyz);
+        assertEquals(decl, xyz.getTypeDeclaration());
+    }
+}
