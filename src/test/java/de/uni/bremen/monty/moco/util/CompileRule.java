878192c4b991b62eb2c49169bcf7b8980b42d227
Simon Taddiken <simon.taddiken@gmail.com>
2015-05-15 10:45:56 +0200

    Improve test execution

--- src/test/java/de/uni/bremen/monty/moco/util/CompileRule.java
+++ src/test/java/de/uni/bremen/monty/moco/util/CompileRule.java
@@ -36,23 +36,33 @@ import de.uni.bremen.monty.moco.visitor.SetParentVisitor;
 import de.uni.bremen.monty.moco.visitor.typeinf.QuantumTypeErasor9k;
 import de.uni.bremen.monty.moco.visitor.typeinf.QuantumTypeResolver3000;
 
-
 public class CompileRule implements TestRule {
 
-    /** Path to folder where generated .monty files will be stored -´ */
-    private static final String TEST_OUTPUT = "target/test-output/type-inf/";
-    private static final String DOT_OUTPUT = "target/dot/type-inf/";
+    /** Path to folder where generated .monty files will be stored */
+    private static final String TEST_OUTPUT = "target/test-output/monty/";
+    /** Path to folder where generated .dot files will be stored */
+    private static final String DOT_OUTPUT = "target/test-output/dot/";
+    /** Path to folder where generated ast pdfs will be stored */
+    private static final String PDF_OUTPUT = "target/test-output/ast";
+    /** Path to folder where genreated llvm code will be stored */
     private static final String LLVM_OUTPUT = "target/test-output/llvm/";
     /** Path to resource folder from which test monty files are read */
     private static final String TEST_DIR = "testTypeInference/";
+
+    private static final long DOT_TIME_OUT = 30; // seconds
+
+    private static final String DOT_EXECUTABLE = "monty.tests.dot";
+    private static final String SKIP_GENERATE_RESOURCES = "monty.tests.skipResources";
 
     private ASTNode ast;
     private Monty monty;
     private TestResource montyResource;
     private String testName;
+    private String namespace;
 
     @Override
     public Statement apply(Statement base, Description description) {
+        this.namespace = getNameSpace(description.getTestClass().getSimpleName());
         this.monty = description.getAnnotation(Monty.class);
         this.montyResource = description.getAnnotation(TestResource.class);
 
@@ -65,9 +75,18 @@ public class CompileRule implements TestRule {
         return base;
     }
 
+    private String getNameSpace(String className) {
+        for (int i = 1; i < className.length(); ++i) {
+            final char c = className.charAt(i);
+            if (Character.isUpperCase(c)) {
+                return className.substring(0, i);
+            }
+        }
+        return className;
+    }
+
     public <T extends ASTNode> T searchFor(Class<T> nodeType, Predicate<T> p) {
         return SearchAST.forNode(nodeType).where(p).in(this.ast).get();
-
     }
 
     public ASTNode typeCheck() throws Exception {
@@ -86,11 +105,11 @@ public class CompileRule implements TestRule {
     public ASTNode compile() throws Exception {
         final Params params = createParams();
         final List<BaseVisitor> additional = Arrays.asList(
-            new QuantumTypeErasor9k(),
-            new ControlFlowVisitor(),
-            new NameManglingVisitor(),
-            new CodeGenerationVisitor(params)
-        );
+                new QuantumTypeErasor9k(),
+                new ControlFlowVisitor(),
+                new NameManglingVisitor(),
+                new CodeGenerationVisitor(params)
+                );
 
         this.ast = createAST(params, additional);
         return this.ast;
@@ -133,6 +152,7 @@ public class CompileRule implements TestRule {
             {
                 setStopOnFirstError(true);
             }
+
             @Override
             protected void onEnterEachNode(ASTNode node) {
                 if (node instanceof Typed) {
@@ -177,18 +197,21 @@ public class CompileRule implements TestRule {
         return mainPackage;
     }
 
-
     private void executeVisitorChain(String testFileName, Params params,
             List<BaseVisitor> additionalVisitors, ASTNode root)
-                    throws Exception {
+            throws Exception {
 
-        final String llvmOutput = LLVM_OUTPUT + getFileName(testFileName) + ".ll";
-        final File llvmDir = new File(LLVM_OUTPUT);
-        if (!llvmDir.exists()) {
-            llvmDir.mkdirs();
+        testFileName = this.namespace + "_" + testFileName;
+        if (skipGenerateResources()) {
+
+        } else {
+            final String llvmOutput = LLVM_OUTPUT + getFileName(testFileName) + ".ll";
+            final File llvmDir = new File(LLVM_OUTPUT);
+            if (!llvmDir.exists()) {
+                llvmDir.mkdirs();
+            }
+            params.setOutputFile(llvmOutput);
         }
-        params.setGenerateOnlyLLVM(true);
-        params.setOutputFile(llvmOutput);
 
         final List<BaseVisitor> visitors = new ArrayList<>();
         visitors.add(new SetParentVisitor());
@@ -203,6 +226,7 @@ public class CompileRule implements TestRule {
                 bv.visitDoubleDispatched(root);
             } catch (MontyBaseException e) {
                 error = e;
+                break;
             } catch (Exception e) {
                 final Position pos = AbstractTypedASTNode.UNKNOWN_POSITION;
                 final String message = pos + " :\n"
@@ -212,18 +236,21 @@ public class CompileRule implements TestRule {
             }
         }
         writeDotFile(testFileName, root);
+        if (params.getInputCode() != null) {
+            writeTestMontyFile(testFileName, params.getInputCode());
+        }
+
         if (error != null) {
             throw error;
-        } else if (params.getInputCode() != null) {
-            writeTestMontyFile(testFileName, params.getInputCode());
         }
     }
 
     private void writeTestMontyFile(String testFileName, String content)
             throws IOException {
-        final String originalFileName = getFileName(testFileName);
-        final String montyFileName = originalFileName + ".monty";
-        final File targetFile = new File(TEST_OUTPUT, montyFileName);
+        if (skipGenerateResources()) {
+            return;
+        }
+        final File targetFile = getNewFile(testFileName, TEST_OUTPUT, ".monty");
 
         if (!targetFile.getParentFile().exists()) {
             targetFile.getParentFile().mkdirs();
@@ -234,10 +261,12 @@ public class CompileRule implements TestRule {
         }
     }
 
-    private void writeDotFile(String testFileName, ASTNode root) throws IOException {
-        final String originalFileName = getFileName(testFileName);
-        final String dotFileName = originalFileName + ".dot";
-        final File targetDotFile = new File(DOT_OUTPUT, dotFileName);
+    private void writeDotFile(String testFileName, ASTNode root)
+            throws IOException, InterruptedException {
+        if (skipGenerateResources()) {
+            return;
+        }
+        final File targetDotFile = getNewFile(testFileName, DOT_OUTPUT, ".dot");
 
         if (!targetDotFile.getParentFile().exists()) {
             targetDotFile.getParentFile().mkdirs();
@@ -246,6 +275,8 @@ public class CompileRule implements TestRule {
         try (final DotVisitor dotVisitor = DotVisitor.toFile(targetDotFile, false)) {
             dotVisitor.visitDoubleDispatched(root);
         }
+
+        createASTasPdf(testFileName, targetDotFile);
     }
 
     private String getFileName(String nameWithExtension) {
@@ -255,5 +286,41 @@ public class CompileRule implements TestRule {
             return nameWithExtension;
         }
         return nameWithExtension.substring(0, dotIdx);
+    }
+
+    private File getNewFile(String testFileName, String rootDir, String newExtension) {
+        newExtension = newExtension.startsWith(".")
+                ? newExtension
+                : "." + newExtension;
+        final File root = new File(rootDir);
+        if (!root.exists()) {
+            root.mkdirs();
+        }
+        final String originalFileName = getFileName(testFileName);
+        final String dotFileName = originalFileName + newExtension;
+        return new File(root, dotFileName);
+    }
+
+    private void createASTasPdf(String testFileName, File dotFile)
+            throws IOException, InterruptedException {
+        final File targetFile = getNewFile(testFileName, PDF_OUTPUT, ".pdf");
+        final String dotExec = getDotExecutable();
+        new ProcessBuilder(
+                dotExec,
+                "-Tpdf",
+                "-o " + targetFile.getName(),
+                dotFile.getAbsolutePath())
+        .directory(targetFile.getParentFile())
+        .inheritIO()
+        .start();
+    }
+
+    private boolean skipGenerateResources() {
+        return Boolean.parseBoolean(System.getProperty(SKIP_GENERATE_RESOURCES, "false"));
+    }
+
+    private String getDotExecutable() {
+        return System.getProperty(DOT_EXECUTABLE,
+                "C:\\Program Files (x86)\\Graphviz 2.28\\bin\\dot.exe");
     }
 }
