5a188ec485bbb47aff255d6ac6f2dcb5f0608c6b
Simon Taddiken <simon.taddiken@gmail.com>
2015-07-23 17:14:21 +0200

    Add some control flow tests

---
+++ src/test/java/de/uni/bremen/monty/moco/visitor/ControlFlowTest.java
@@ -0,0 +1,88 @@
+package de.uni.bremen.monty.moco.visitor;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import de.uni.bremen.monty.moco.exception.InvalidControlFlowException;
+import de.uni.bremen.monty.moco.util.CompileRule;
+import de.uni.bremen.monty.moco.util.Monty;
+
+public class ControlFlowTest {
+
+    @Rule
+    public final CompileRule compiler = new CompileRule();
+    @Rule
+    public final ExpectedException exception = ExpectedException.none();
+    
+    @Test
+    @Monty(
+    "String foo(Bool cond):\n" +
+    "    if cond:\n" +
+    "        return \"a\""
+    )
+    public void testMissingElseBranch() throws Exception {
+        exception.expect(InvalidControlFlowException.class);
+        compiler.compile();
+    }
+    
+    @Test
+    @Monty(
+    "String foo(Bool cond):\n" +
+    "    if cond:\n" +
+    "        pass\n" +
+    "    else:\n" +
+    "        return \"a\""
+    )
+    public void testMissingIfBranch() throws Exception {
+        exception.expect(InvalidControlFlowException.class);
+        compiler.compile();
+    }
+    
+    @Test
+    @Monty(
+    "String foo(Bool cond):\n" +
+    "    if cond:\n" +
+    "        if not cond:\n" +
+    "            return \"c\"\n" +
+    "        else:\n" +
+    "            pass\n"+
+    "    else:\n" +
+    "        return \"a\""
+    )
+    public void testMissingInNestedElseStatements() throws Exception {
+        exception.expect(InvalidControlFlowException.class);
+        compiler.compile();
+    }
+    
+    @Test
+    @Monty(
+    "String foo(Bool cond):\n" +
+    "    if cond:\n" +
+    "        if not cond:\n" +
+    "            pass\n"+
+    "        else:\n" +
+    "            return \"c\"\n" +
+    "    else:\n" +
+    "        return \"a\""
+    )
+    public void testMissingInNestedIfStatements() throws Exception {
+        exception.expect(InvalidControlFlowException.class);
+        compiler.compile();
+    }
+    
+    @Test
+    @Monty(
+    "String foo(Bool cond):\n" +
+    "    if cond:\n" +
+    "        if not cond:\n" +
+    "            return \"b\"\n"+
+    "        else:\n" +
+    "            return \"c\"\n" +
+    "    else:\n" +
+    "        return \"a\""
+    )
+    public void testNestedIfStatementsCorrectFlow() throws Exception {
+        compiler.compile();
+    }
+}
